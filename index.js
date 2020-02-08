5f18ce23836ac85e96111afd021f2c5ae097c41d
rogulinilya <rogulinilya@gmail.com>
2020-02-08 19:26:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function asyncForEach(array, callback) {
 	}
 }
 
-bot_VK.event("message_new", async (ctx) => {
+bot_VK.event("message_new", async ctx => {
 	const id_vk = ctx.message.from_id;
 	const user = await User.find({ id_vk });
 	const permission = user[0] ? user[0].permission : false; 
@@ -102,7 +102,7 @@ bot_VK.event("message_new", async (ctx) => {
 });
 
 
-bot_VK.event("group_join", async (ctx) => {
+bot_VK.event("group_join", async ctx => {
 	const id_vk = ctx.message.user_id;
 	const user = await User.find({ id_vk });
 	if (!user[0]) {
@@ -112,8 +112,6 @@ bot_VK.event("group_join", async (ctx) => {
 		}).then(res => res.response[0]);
 		const new_user = new User({id_vk, id_telegram: null, name, surname, permission: true, songs: [null, null, null]});
 		const hash = md5(id_vk + process.env.SALT).substr(0, 10);
-		console.log("qwe");
-		console.log(hash);
 		ctx.reply({message: `Привет, авторизуйся в телеграме, чтобы ты смог получать аудиозаписи\n\nt-do.ru/ilushaR_bot?start=${id_vk}-${hash}`, 
 			random_id: Date.now(), dont_parse_links: 1 });
 		await new_user.save();
@@ -131,18 +129,17 @@ bot_telegram.onText(/\/start/, async msg => {
 	const id_telegram = msg.chat.id;
 	const message = msg.text.slice(7).split("-");
 	const [id_vk, hash] = message;
-	console.log(hash);
 	if (md5(id_vk + process.env.SALT).substr(0, 10) !== hash) {
 		return bot_telegram.sendMessage(id_telegram, "Нет доступа");
 	}
 	const user = await User.find({ id_telegram });
-	console.log(user[0]);
 	if (user[0]) {
 		return bot_telegram.sendMessage(id_telegram, "Ты уже добавлен");
 	}
 	await User.updateOne({ id_vk }, { $set: { id_telegram }});
 	bot_telegram.sendMessage(id_telegram, "Ты добавлен, теперь можешь получать музыку");
 	const { songs } = await User.findOne({ id_vk }, {songs: 1});
+	console.log(songs);
 	const finished = util.promisify(stream.finished);
 	asyncForEach(songs, async ({url, artist, title}, index) => {
 		const file = fs.createWriteStream(`audio${index}.mp3`);
